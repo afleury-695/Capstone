1887b34ae208777793237f0cf08367068d0662f5
Cecilia Ames <afleury@argyletech.net>
2019-09-10 09:52:41 -0500

    Committing first creation of backend java file to ensure repo communicating

---
+++ CapstoneBackend/src/test/java/com/company/CapstoneBackend/CapstoneBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.CapstoneBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapstoneBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
